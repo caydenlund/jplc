11404c0fb71210d8cafd467ecde6b32653822149
Cayden Lund <cayden.lund@utah.edu>
2023-01-18 14:29:02 -0700

    Write header for the lexer.

---
+++ lexer/lexer.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file lexer.hpp
+ * @package Assignment 2
+ * @author Cayden Lund (u1182408)
+ * @brief Defines the lexing functions.
+ *
+ */
+
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+#include <functional>
+#include <regex>
+#include <tuple>
+#include <utility>
+
+#include "token/token.hpp"
+
+namespace lexer {
+    /**
+     * @brief Defines the `lexer` functor class.
+     * @details An instance is used to lex a particular case of input.
+     *
+     */
+    class lexer {
+    private:
+        /**
+         * @brief The regular expression used to match input.
+         *
+         */
+        const std::regex pattern;
+
+        /**
+         * @brief The token type to match.
+         *
+         */
+        const token::token_type type;
+
+    public:
+        /**
+         * @brief Constructor.
+         *
+         * @param pattern The regular expression used to match input.
+         */
+        lexer(const std::string& pattern, token::token_type type);
+
+        /**
+         * @brief Function call operator.
+         * @details Call the instance as a function.
+         *
+         * @param input The input string to read.
+         * @param index The starting position from which to read.
+         * @return A pair of the lexed token and the new starting position.
+         */
+        std::tuple<token::token, unsigned int> operator()(const std::string& input, unsigned int index);
+    };
+
+    /**
+     * @brief Assembles the set of lexers in the correct order.
+     *
+     * @return The set of lexers to use, in the correct order.
+     */
+    std::vector<lexer> assemble_lexers();
+
+    /**
+     * @brief Given the input string, lexes it in its entirety.
+     *
+     * @param input The input string.
+     * @return The series of tokens composing the input string.
+     */
+    std::vector<token::token> lex_all(const std::string& input);
+}  //  namespace lexer
+
+#endif
