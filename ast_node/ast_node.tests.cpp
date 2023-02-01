ca260b379bb0ef06ec185189418468d67fc178ec
Cayden Lund <cayden.lund@utah.edu>
2023-02-01 14:35:23 -0700

    Add tests for the new AST nodes.

--- ast_node/ast_node.tests.cpp
+++ ast_node/ast_node.tests.cpp
@@ -8,6 +8,7 @@
 
 #include "ast_node.hpp"
 #include "tests/tests.hpp"
+#include <sstream>
 
 namespace tests::ast_node_tests {
     /**
@@ -21,21 +22,6 @@ namespace tests::ast_node_tests {
     inline std::string str_cmp(const std::string& actual, const std::string& expected) {
         if (actual == expected) return "";
         return "Result was '" + actual + "' instead of '" + expected + "'";
-    }
-
-    /**
-     * @brief Ensures that the `ast_node` constructors do not throw an exception.
-     *
-     * @return The empty string if successful; an exception is thrown otherwise.
-     */
-    std::string constructor_no_fail() {
-        [[maybe_unused]] const ast_node::ast_node ast(ast_node::node_type::CMD);
-        [[maybe_unused]] const ast_node::cmd_node cmd;
-
-        const token::token variable {0, "xyz", token::token_type::VARIABLE};
-        const ast_node::variable_argument_node variable_argument(variable);
-
-        return "";
     }
 
     /**
@@ -64,13 +50,58 @@ namespace tests::ast_node_tests {
         std::string result = str_cmp(argument.s_expression(), "");
         if (result != "") return result;
 
+        //  `array_argument_node`:
+        //  ----------------------
+        const token::token variable {0, "xyz", token::token_type::VARIABLE};
+        const std::vector<token::token> vars = {{0, "a", token::token_type::VARIABLE},
+                                                {0, "b", token::token_type::VARIABLE},
+                                                {0, "c", token::token_type::VARIABLE}};
+
+        const ast_node::array_argument_node array_argument(variable, vars);
+        result = str_cmp(array_argument.s_expression(), "(ArrayArgument xyz a b c)");
+        if (result != "") return result;
+
         //  `variable_argument_node`:
         //  -------------------------
-        const token::token variable {0, "xyz", token::token_type::VARIABLE};
-
         const ast_node::variable_argument_node variable_argument(variable);
         result = str_cmp(variable_argument.s_expression(), "(VarArgument xyz)");
         return result;
+    }
+
+    /**
+     * @brief Ensures that binding AST nodes return the correct s-expression.
+     *
+     * @return The empty string if successful; an error message otherwise.
+     */
+    std::string binding_s_expressions() {
+        //  `binding_node`:
+        //  ---------------
+        const ast_node::binding_node binding;
+        std::string result = str_cmp(binding.s_expression(), "");
+        if (result != "") return result;
+
+        //  `var_binding_node`:
+        //  -------------------
+        //  Note: we do this one before the next one because we need to check that its s-expression works.
+        const token::token variable {0, "xyz", token::token_type::VARIABLE};
+        const std::shared_ptr<ast_node::argument_node> argument = std::make_shared<ast_node::variable_argument_node>(
+                variable);
+        const std::shared_ptr<ast_node::type_node> type = std::make_shared<ast_node::bool_type_node>();
+
+        const ast_node::var_binding_node var_binding(argument, type);
+        result = str_cmp(var_binding.s_expression(), "(VarBinding (VarArgument xyz) (BoolType))");
+        if (result != "") return result;
+
+        //  `tuple_binding_node`:
+        //  ---------------------
+        const std::vector<std::shared_ptr<ast_node::binding_node>> bindings {
+                std::make_shared<ast_node::var_binding_node>(argument, type)};
+
+        const ast_node::tuple_binding_node tuple_binding(bindings);
+        result = str_cmp(tuple_binding.s_expression(), "(TupleBinding (VarBinding (VarArgument xyz) (BoolType)))");
+        if (result != "") return result;
+
+        return "";
     }
 
     /**
@@ -94,9 +125,37 @@ namespace tests::ast_node_tests {
         result = str_cmp(assert_cmd.s_expression(), R"((AssertCmd (TrueExpr) "file.png"))");
         if (result != "") return result;
 
+        //  `fn_cmd_node`:
+        //  --------------
+        const token::token variable {0, "xyz", token::token_type::VARIABLE};
+        const token::token variable_2 {0, "abc", token::token_type::VARIABLE};
+        const token::token variable_3 {0, "qrs", token::token_type::VARIABLE};
+        const std::vector<std::shared_ptr<ast_node::binding_node>> bindings {
+                std::make_shared<ast_node::var_binding_node>(
+                        std::make_shared<ast_node::variable_argument_node>(variable_2),
+                        std::make_shared<ast_node::bool_type_node>()),
+                std::make_shared<ast_node::var_binding_node>(
+                        std::make_shared<ast_node::variable_argument_node>(variable_3),
+                        std::make_shared<ast_node::int_type_node>())};
+        const std::shared_ptr<ast_node::type_node> return_type = std::make_shared<ast_node::bool_type_node>();
+        const std::vector<std::shared_ptr<ast_node::stmt_node>> statements {
+                std::make_shared<ast_node::let_stmt_node>(
+                        std::make_shared<ast_node::argument_lvalue_node>(
+                                std::make_shared<ast_node::variable_argument_node>(variable_2)),
+                        std::make_shared<ast_node::true_expr_node>()),
+                std::make_shared<ast_node::return_stmt_node>(std::make_shared<ast_node::true_expr_node>())};
+
+        const ast_node::fn_cmd_node fn_cmd(variable, bindings, return_type, statements);
+        result = str_cmp(fn_cmd.s_expression(),
+                         "(FnCmd xyz "
+                         "((VarBinding (VarArgument abc) (BoolType)) (VarBinding (VarArgument qrs) (IntType))) "
+                         "(BoolType) "
+                         "(LetStmt (ArgLValue (VarArgument abc)) (TrueExpr)) "
+                         "(ReturnStmt (TrueExpr)))");
+        if (result != "") return result;
+
         //  `let_cmd_node`:
         //  ---------------
-        const token::token variable {0, "xyz", token::token_type::VARIABLE};
         const std::shared_ptr<ast_node::variable_argument_node> variable_argument
                 = std::make_shared<ast_node::variable_argument_node>(variable);
         const std::shared_ptr<ast_node::argument_lvalue_node> argument_lvalue
@@ -122,6 +181,14 @@ namespace tests::ast_node_tests {
         //  ----------------
         const ast_node::show_cmd_node show_cmd(true_expr);
         result = str_cmp(show_cmd.s_expression(), R"((ShowCmd (TrueExpr)))");
+        if (result != "") return result;
+
+        //  `time_cmd_node`:
+        //  ----------------
+        std::shared_ptr<ast_node::cmd_node> command = std::make_shared<ast_node::show_cmd_node>(true_expr);
+
+        const ast_node::time_cmd_node time_cmd(command);
+        result = str_cmp(time_cmd.s_expression(), R"((TimeCmd (ShowCmd (TrueExpr))))");
         if (result != "") return result;
 
         //  `type_cmd_node`:
@@ -154,6 +221,31 @@ namespace tests::ast_node_tests {
         std::string result = str_cmp(expr.s_expression(), "");
         if (result != "") return result;
 
+        //  `array_index_expr_node`:
+        //  ------------------------
+        const token::token variable {0, "xyz", token::token_type::VARIABLE};
+        const std::vector<std::shared_ptr<ast_node::expr_node>> index_params {
+                std::make_shared<ast_node::true_expr_node>(), std::make_shared<ast_node::false_expr_node>()};
+
+        const ast_node::array_index_expr_node array_index_expr(variable, index_params);
+        result = str_cmp(array_index_expr.s_expression(), R"((ArrayIndexExpr xyz (TrueExpr) (FalseExpr)))");
+        if (result != "") return result;
+
+        //  `array_literal_expr_node`:
+        //  --------------------------
+        const std::vector<std::shared_ptr<ast_node::expr_node>> literal_params {
+                std::make_shared<ast_node::true_expr_node>(), std::make_shared<ast_node::false_expr_node>()};
+
+        const ast_node::array_literal_expr_node array_literal_expr(literal_params);
+        result = str_cmp(array_literal_expr.s_expression(), R"((ArrayLiteralExpr (TrueExpr) (FalseExpr)))");
+        if (result != "") return result;
+
+        //  `call_expr_node`:
+        //  -----------------
+        const ast_node::call_expr_node call_expr(variable, literal_params);
+        result = str_cmp(call_expr.s_expression(), R"((CallExpr xyz (TrueExpr) (FalseExpr)))");
+        if (result != "") return result;
+
         //  `false_expr_node`:
         //  ------------------
         const ast_node::false_expr_node false_expr;
@@ -163,6 +255,7 @@ namespace tests::ast_node_tests {
         //  `float_expr_node`:
         //  ------------------
         const token::token float_val {0, "3.", token::token_type::FLOATVAL};
+
         const ast_node::float_expr_node float_expr(float_val);
         result = str_cmp(float_expr.s_expression(), R"((FloatExpr 3))");
         if (result != "") return result;
@@ -170,6 +263,7 @@ namespace tests::ast_node_tests {
         //  `integer_expr_node`:
         //  --------------------
         const token::token int_val {0, "3", token::token_type::INTVAL};
+
         const ast_node::integer_expr_node integer_expr(int_val);
         result = str_cmp(integer_expr.s_expression(), R"((IntExpr 3))");
         if (result != "") return result;
@@ -180,9 +274,22 @@ namespace tests::ast_node_tests {
         result = str_cmp(true_expr.s_expression(), R"((TrueExpr))");
         if (result != "") return result;
 
+        //  `tuple_index_expr_node`:
+        //  ------------------------
+        const std::shared_ptr<ast_node::expr_node> true_expr_ptr = std::make_shared<ast_node::true_expr_node>();
+
+        const ast_node::tuple_index_expr_node tuple_index_expr(true_expr_ptr, 1);
+        result = str_cmp(tuple_index_expr.s_expression(), R"((TupleIndexExpr (TrueExpr) 1))");
+        if (result != "") return result;
+
+        //  `tuple_literal_expr_node`:
+        //  --------------------------
+        const ast_node::tuple_literal_expr_node tuple_literal_expr(literal_params);
+        result = str_cmp(tuple_literal_expr.s_expression(), R"((TupleLiteralExpr (TrueExpr) (FalseExpr)))");
+        if (result != "") return result;
+
         //  `variable_expr_node`:
         //  ---------------------
-        const token::token variable {0, "xyz", token::token_type::VARIABLE};
         const ast_node::variable_expr_node variable_expr(variable);
         result = str_cmp(variable_expr.s_expression(), R"((VarExpr xyz))");
         return result;
@@ -208,6 +315,62 @@ namespace tests::ast_node_tests {
 
         const ast_node::argument_lvalue_node argument_lvalue(variable_argument);
         result = str_cmp(argument_lvalue.s_expression(), "(ArgLValue (VarArgument xyz))");
+        if (result != "") return result;
+
+        //  `tuple_lvalue_node`:
+        //  --------------------
+        const std::vector<std::shared_ptr<ast_node::lvalue_node>> lvalues {
+                std::make_shared<ast_node::argument_lvalue_node>(std::make_shared<ast_node::variable_argument_node>(
+                        token::token {0, "abc", token::token_type::VARIABLE})),
+                std::make_shared<ast_node::argument_lvalue_node>(std::make_shared<ast_node::variable_argument_node>(
+                        token::token {0, "xyz", token::token_type::VARIABLE}))};
+
+        const ast_node::tuple_lvalue_node tuple_lvalue(lvalues);
+        result = str_cmp(tuple_lvalue.s_expression(),
+                         "(TupleLValue (ArgLValue (VarArgument abc)) (ArgLValue (VarArgument xyz)))");
+        return result;
+    }
+
+    /**
+     * @brief Ensures that statement AST nodes return the correct s-expression.
+     *
+     * @return The empty string if successful; an error message otherwise.
+     */
+    std::string statement_s_expressions() {
+        //  `stmt_node`:
+        //  ------------
+        const ast_node::stmt_node stmt;
+        std::string result = str_cmp(stmt.s_expression(), "");
+        if (result != "") return result;
+
+        //  `assert_stmt_node`:
+        //  -------------------
+        const std::shared_ptr<ast_node::true_expr_node> true_expr = std::make_shared<ast_node::true_expr_node>();
+        const token::string_token file {{0, R"("file.png")", token::token_type::VARIABLE}, "file.png"};
+
+        const ast_node::assert_stmt_node assert_stmt(true_expr, file);
+        result = str_cmp(assert_stmt.s_expression(), R"((AssertStmt (TrueExpr) "file.png"))");
+        if (result != "") return result;
+
+        //  `let_stmt_node`:
+        //  ----------------
+        const token::token variable {0, "xyz", token::token_type::VARIABLE};
+        const std::shared_ptr<ast_node::variable_argument_node> variable_argument
+                = std::make_shared<ast_node::variable_argument_node>(variable);
+        const std::shared_ptr<ast_node::argument_lvalue_node> argument_lvalue
+                = std::make_shared<ast_node::argument_lvalue_node>(variable_argument);
+
+        const ast_node::let_stmt_node let_stmt(argument_lvalue, true_expr);
+        result = str_cmp(let_stmt.s_expression(), R"((LetStmt (ArgLValue (VarArgument xyz)) (TrueExpr)))");
+        if (result != "") return result;
+
+        //  `return_stmt_node`:
+        //  -------------------
+        const std::shared_ptr<ast_node::expr_node> return_val = std::make_shared<ast_node::variable_expr_node>(
+                variable);
+
+        const ast_node::return_stmt_node return_stmt(return_val);
+        result = str_cmp(return_stmt.s_expression(), R"((ReturnStmt (VarExpr xyz)))");
         return result;
     }
 
@@ -221,6 +384,14 @@ namespace tests::ast_node_tests {
         //  ------------
         const ast_node::type_node type;
         std::string result = str_cmp(type.s_expression(), "");
+        if (result != "") return result;
+
+        //  `array_type_node`:
+        //  ------------------
+        const std::shared_ptr<ast_node::type_node> array_main_type = std::make_shared<ast_node::bool_type_node>();
+
+        const ast_node::array_type_node array_type(array_main_type, 2);
+        result = str_cmp(array_type.s_expression(), "(ArrayType (BoolType) 2)");
         if (result != "") return result;
 
         //  `bool_type_node`:
@@ -241,6 +412,16 @@ namespace tests::ast_node_tests {
         result = str_cmp(int_type.s_expression(), "(IntType)");
         if (result != "") return result;
 
+        //  `tuple_type_node`:
+        //  ------------------
+        const std::vector<std::shared_ptr<ast_node::type_node>> type_nodes {
+                std::make_shared<ast_node::bool_type_node>(), std::make_shared<ast_node::float_type_node>(),
+                std::make_shared<ast_node::int_type_node>()};
+
+        const ast_node::tuple_type_node tuple_type(type_nodes);
+        result = str_cmp(tuple_type.s_expression(), "(TupleType (BoolType) (FloatType) (IntType))");
+        if (result != "") return result;
+
         //  `variable_type_node`:
         //  ---------------------
         const token::token variable {0, "xyz", token::token_type::VARIABLE};
@@ -257,13 +438,14 @@ namespace tests::ast_node_tests {
      */
     std::vector<test_t> get_all_tests() {
         std::vector<test_t> tests;
-        tests.emplace_back(constructor_no_fail, "`ast_node` constructor: no-fail");
         tests.emplace_back(variable_argument_node_constructor_exception,
                            "`variable_argument_node` constructor: exception");
         tests.emplace_back(argument_s_expressions, "Argument s-expressions");
+        tests.emplace_back(binding_s_expressions, "Binding s-expressions");
         tests.emplace_back(command_s_expressions, "Command s-expressions");
         tests.emplace_back(expression_s_expressions, "Expression s-expressions");
         tests.emplace_back(lvalue_s_expressions, "Lvalue s-expressions");
+        tests.emplace_back(statement_s_expressions, "Statement s-expressions");
         tests.emplace_back(type_s_expressions, "Type s-expressions");
         return tests;
     }
